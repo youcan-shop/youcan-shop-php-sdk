0d9322fdc5245ab18e0dde9c72e2a86bff7093f2
RAFIE Younes <younes.rafie@gmail.com>
2024-01-09 11:08:27 +0100

    Fix wrong dev URL

--- src/Constants.php
+++ src/Constants.php
@@ -5,7 +5,7 @@ namespace YouCan\Shop\Sdk;
 class Constants
 {
     public const LIVE_API_URL = 'https://api.youcan.shop/';
-    public const DEV_API_URL = 'https://api.youcandev.shop/';
+    public const DEV_API_URL = 'https://api.youcanshop.dev/';
     public const LIVE_SELLER_AREA_URL = 'https://seller-area.youcan.shop/';
-    public const DEV_SELLER_AREA_URL = 'https://seller-area.youcandev.shop/';
+    public const DEV_SELLER_AREA_URL = 'https://seller-area.youcanshop.dev/';
 }
